0a829e552081fb27fe3e62e61f1a9ac6bf7be6bc
Luiz Carvalho <luiz@ic.ufal.br>
2017-10-12 19:28:05 -0300

    Remove a bug in RFIC

--- src/main/java/br/edu/ufal/ic/easy/cppmt/mutation/operation/RFIC.java
+++ src/main/java/br/edu/ufal/ic/easy/cppmt/mutation/operation/RFIC.java
@@ -34,13 +34,14 @@ public class RFIC implements MutationOperator {
 			NodeList lIfChildNodes = ifNode.getChildNodes();
 			for (int j = 0; j < lIfChildNodes.getLength(); ++j) {
 				if (lIfChildNodes.item(j).getNodeName().equals("expr")) {
-					searchAndRemove(document, lIfChildNodes.item(j));
-					Mutation mutation = new Mutation(document, originalDocument, this, id++);
-					mutation.writeToFile(originalFile);
-					System.out.println("mutation: " + mutation.getMutationFile().getAbsolutePath());
-					document = DocumentClone.clone(originalDocument);
-					lNodes = document.getDocumentElement().getElementsByTagName("cpp:if");
-					break;
+					if (searchAndRemove(document, lIfChildNodes.item(j))) {
+						Mutation mutation = new Mutation(document, originalDocument, this, id++);
+						mutation.writeToFile(originalFile);
+						System.out.println("mutation: " + mutation.getMutationFile().getAbsolutePath());
+						document = DocumentClone.clone(originalDocument);
+						lNodes = document.getDocumentElement().getElementsByTagName("cpp:if");
+						break;
+					}
 				}
 			}
 		}
@@ -50,26 +51,40 @@ public class RFIC implements MutationOperator {
 	 * Search for call and operator and remove without first call;
 	 * @param doc Document
 	 * @param expr node from 'cpp:if' node
+	 * @return if removed true. Otherwise false.
 	 */
-	private void searchAndRemove(Document doc, Node expr) {
+	private boolean searchAndRemove(Document doc, Node expr) {
 		NodeList childNodes = expr.getChildNodes();
 		int callCount = 0;
 		int operatorCount = 0;
+		Node callNode = null;
+		Node opNode = null;
+		int callIndex = -1;
+		int opIndex = -1;
 		for (int i = 0; i < childNodes.getLength(); ++i) {
 			Node node = childNodes.item(i);
-			//if (node.getNodeName().equals("call")) {
 			if (node.getNodeName().equals("call") || node.getNodeName().equals("name")) {
 				++callCount;
-				if (callCount == 2) {
-					expr.removeChild(node);
+				if (callCount >= 2) {
+					callNode = node;
+					callIndex = i;
 				}
 			} else if (node.getNodeName().equals("operator")) {
 				++operatorCount;
-				if (operatorCount == 1) {
-					expr.removeChild(node);
+				if (operatorCount >= 1) {
+					opNode = node;
+					opIndex = i;
 				}
 			}
 		}
+		if (callNode != null && opNode != null && callIndex > opIndex) {
+			expr.removeChild(callNode);
+			expr.removeChild(opNode);
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
 
 	@Override
